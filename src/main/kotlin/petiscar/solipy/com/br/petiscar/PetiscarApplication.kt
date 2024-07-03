6dd94eadca70bda4e3417d92fcdee6207dcfe236
Pedro Silva <pedro@dimensiva.com.br>
2024-07-03 18:51:21 -0300

    Primeiro commit

---
+++ src/main/kotlin/petiscar/solipy/com/br/petiscar/PetiscarApplication.kt
@@ -0,0 +1,11 @@
+package petiscar.solipy.com.br.petiscar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PetiscarApplication
+
+fun main(args: Array<String>) {
+	runApplication<PetiscarApplication>(*args)
+}
---
+++ src/test/kotlin/petiscar/solipy/com/br/petiscar/PetiscarApplicationTests.kt
@@ -0,0 +1,13 @@
+package petiscar.solipy.com.br.petiscar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PetiscarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
